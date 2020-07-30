1435d10433368b9d6416c9f4f942d530bec846d8
ProYuan <50544216@qq.com>
2020-07-30 23:39:18 +0800

    343.整数拆分

---
+++ 每日一题/343.整数拆分.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode.cn id=343 lang=python3
+#
+# [343] 整数拆分
+#
+
+# @lc code=start
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        if n <= 3:
+            return n - 1
+        quotient, remainder = n // 3, n % 3
+        if remainder == 0:
+            return 3 ** quotient
+        elif remainder == 1:
+            return 3 ** (quotient - 1) * 4
+        else:
+            return 3 ** quotient * 2
+
+# @lc code=end
+
