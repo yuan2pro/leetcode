5a2fec4348b2f59d45afc82a7b1387f29b13704b
ProYuan <50544216@qq.com>
2020-10-15 20:19:30 +0800

    116

---
+++ Python/116.填充每个节点的下一个右侧节点指针.py
@@ -0,0 +1,36 @@
+#
+# @lc app=leetcode.cn id=116 lang=python3
+#
+# [116] 填充每个节点的下一个右侧节点指针
+#
+
+# @lc code=start
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+import collections
+
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        if not root:
+            return root
+        Q = collections.deque([root])
+        while Q:
+            size = len(Q)
+            for i in range(size):
+                node = Q.popleft()
+                if i < size - 1:
+                    node.next = Q[0]
+                if node.left:
+                    Q.append(node.left)
+                if node.right:
+                    Q.append(node.right)
+        return root
+# @lc code=end
