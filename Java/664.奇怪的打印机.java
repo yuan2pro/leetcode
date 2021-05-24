b673747a5a0f94234625047a2c1e28893fe3d3ce
ProYuan <50544216@qq.com>
2021-05-24 20:38:27 +0800

    664

---
+++ Java/664.奇怪的打印机.java
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=664 lang=java
+ *
+ * [664] 奇怪的打印机
+ */
+
+// @lc code=start
+class Solution {
+    public int strangePrinter(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i][j - 1];
+                } else {
+                    int minn = Integer.MAX_VALUE;
+                    for (int k = i; k < j; k++) {
+                        minn = Math.min(minn, dp[i][k] + dp[k + 1][j]);
+                    }
+                    dp[i][j] = minn;
+                }
+            }
+        }
+        return dp[0][n - 1];
+    }
+}
+// @lc code=end
