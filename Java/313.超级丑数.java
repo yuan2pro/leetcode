357e2245b36196de083599a3c225990865d0a204
William_Yuan <12624341+yw0822@users.noreply.github.com>
2021-08-10 09:01:01 +0800

    313

---
+++ Java/313.超级丑数.java
@@ -0,0 +1,65 @@
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+/*
+ * 
+ * 　　┏┓　　　┏┓+ +
+ * 　┏┛┻━━━┛┻┓ + +
+ * 　┃　　　　　　　┃ 　
+ * 　┃　　　━　　　┃ ++ + + +
+ *  ████━████ ┃+
+ * 　┃　　　　　　　┃ +
+ * 　┃　　　┻　　　┃
+ * 　┃　　　　　　　┃ + +
+ * 　┗━┓　　　┏━┛
+ * 　　　┃　　　┃　　　　　　　　　　　
+ * 　　　┃　　　┃ + + + +
+ * 　　　┃　　　┃
+ * 　　　┃　　　┃ +  神兽保佑
+ * 　　　┃　　　┃    代码无bug　　
+ * 　　　┃　　　┃　　+　　　　　　　　　
+ * 　　　┃　 　　┗━━━┓ + +
+ * 　　　┃ 　　　　　　　┣┓
+ * 　　　┃ 　　　　　　　┏┛
+ * 　　　┗┓┓┏━┳┓┏┛ + + + +
+ * 　　　　┃┫┫　┃┫┫
+ * 　　　　┗┻┛　┗┻┛+ + + +
+ * 
+ * 
+ * @Author       : William
+ * @Date         : 2021-08-09 14:48:40
+ * @LastEditTime : 2021-08-09 23:51:29
+ * @LastEditors  : Do not edit
+ * @FilePath     : /Java/313.超级丑数.java
+ * @Description  : 
+ */
+
+/*
+ * @lc app=leetcode.cn id=313 lang=java
+ *
+ * [313] 超级丑数
+ */
+
+// @lc code=start
+class Solution {
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        Set<Long> seen = new HashSet<>();
+        PriorityQueue<Long> heap = new PriorityQueue<>();
+        seen.add(1L);
+        heap.offer(1L);
+        int ugly = 0;
+        for (int i = 0; i < n; i++) {
+            long curr = heap.poll();
+            ugly = (int) curr;
+            for (int prime : primes) {
+                long next = curr * prime;
+                if (seen.add(next)) {
+                    heap.offer(next);
+                }
+            }
+        }
+        return ugly;
+    }
+}
+// @lc code=end
