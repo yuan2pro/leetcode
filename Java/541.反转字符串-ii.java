b1b2a918c7f1a0c5187794f77fa0ca80cc1ed7cb
William_Yuan <12624341+yw0822@users.noreply.github.com>
2021-08-20 19:35:27 +0800

    541

---
+++ Java/541.反转字符串-ii.java
@@ -0,0 +1,47 @@
+/*
+ *           佛曰:  
+ *                   写字楼里写字间，写字间里程序员；  
+ *                   程序人员写程序，又拿程序换酒钱。  
+ *                   酒醒只在网上坐，酒醉还来网下眠；  
+ *                   酒醉酒醒日复日，网上网下年复年。  
+ *                   但愿老死电脑间，不愿鞠躬老板前；  
+ *                   奔驰宝马贵者趣，公交自行程序员。  
+ *                   别人笑我忒疯癫，我笑自己命太贱；  
+ *                   不见满街漂亮妹，哪个归得程序员？
+ * 
+ * @Author       : William
+ * @Date         : 2021-08-20 19:21:43
+ * @LastEditTime : 2021-08-20 19:34:46
+ * @LastEditors  : Do not edit
+ * @FilePath     : /undefined/Users/william/leetcode/Java/541.反转字符串-ii.java
+ * @Description  : 
+ */
+
+/*
+ * @lc app=leetcode.cn id=541 lang=java
+ *
+ * [541] 反转字符串 II
+ */
+
+// @lc code=start
+class Solution {
+    public String reverseStr(String s, int k) {
+        int n = s.length();
+        char[] arr = s.toCharArray();
+        for (int i = 0; i < n; i = i + 2 * k) {
+            reverse(arr, i, Math.min(i + k, n) - 1);
+        }
+        return new String(arr);
+    }
+
+    public void reverse(char[] arr, int left, int right) {
+        while (left < right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
+// @lc code=end
