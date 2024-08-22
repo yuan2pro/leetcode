f6c2c2de45ed9cca28bd25ef4ee65599e82ddcd6
yw0822 <12624341+yw0822@users.noreply.github.com>
2024-08-23 00:39:08 +0800

    Medium 322 & 518

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -206,6 +206,33 @@ public class Solution {
         return index;
     }
 
+    // 322. Coin Change
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+
+    // 518. Coin Change II
+    public static int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
+    }
+
     public static void main(String[] args) {
     }
 }
